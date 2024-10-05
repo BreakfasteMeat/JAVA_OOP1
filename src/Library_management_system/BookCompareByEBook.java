7713e121f2ec9e5f8e5e7f77f1fcc176cee735e0
Seth Nathaniel <emiaseth@gmail.com>
2024-10-05 15:24:12 +0800

    Added sorting by if EBook

---
+++ src/Library_management_system/BookCompareByEBook.java
@@ -0,0 +1,26 @@
+package Library_management_system;
+
+import java.util.Comparator;
+
+public class BookCompareByEBook implements Comparator<Book>{
+	@Override
+	public int compare(Book o1, Book o2){
+		int o1IsEbook;
+		int o2IsEbook;
+		if(o1 instanceof EBook){
+			o1IsEbook = 1;
+		} else {
+			o1IsEbook = 0;
+		}
+		if(o2 instanceof  EBook){
+			o2IsEbook = 1;
+		} else {
+			o2IsEbook = 0;
+		}
+		int ret = o1IsEbook - o2IsEbook;
+		if(ret == 0){
+			ret = o1.getName().compareTo(o2.getName());
+		}
+		return ret;
+	}
+}
